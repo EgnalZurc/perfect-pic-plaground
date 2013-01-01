784724be406435693625345f88a1e255e13b0a94
vogonpoet42 <vogonpoet42@29f8fe4d-d83e-4efa-2f1a-eb354652e9c9>
2013-01-01 19:28:15 +0000

    Streamlined picture generation and added animation capabilities.

--- Serial Sign Driver/bmp.py
+++ Serial Sign Driver/bmp.py
@@ -36,33 +36,50 @@ def processRowsPart(imag_rows,widthSection,index):
         rows.append(rowbinary)
     return rows
 
-#Split section of BMP file into pixel rows and process into binary
-def makePicturePart(path,widthTotal,widthSection,sectionIndex):
-    im = Image.open(path)
-    imag = list(im.getdata())
-    imag_rows = []
-    while imag:
-        imag_row = imag[:widthTotal]
-        imag_rows.append(imag_row)
-        imag = imag[widthTotal:]
-    print processRowsPart(imag_rows,widthSection,sectionIndex)
+#Rotates each row in the collection of rows
+def rotateEachRowOnce(imag_rows):
+    new_imag_rows = []
+    for imag_row in imag_rows:
+        new_imag_row = imag_row[1:] + imag_row[:1]
+        new_imag_rows.append(new_imag_row)
+    return new_imag_rows
 
-#Split BMP file into pixel rows and process into binary
-def makePicture(path,width):
-    im = Image.open(path)
-    imag = list(im.getdata())
+#Takes the long list of the image pixels and splits it into individual rows
+def splitToRows(imag,width):
     imag_rows = []
     while imag:
         imag_row = imag[:width]
         imag_rows.append(imag_row)
         imag = imag[width:]
-    print processRows(imag_rows)
+    return imag_rows
+
+#Split section of BMP file into pixel rows and process into binary
+def makePicturePart(path,widthTotal,widthSection,sectionIndex):
+    im = Image.open(path)
+    imag = list(im.getdata())
+    imag_rows = splitToRows(imag,widthTotal)
+    return processRowsPart(imag_rows,widthSection,sectionIndex)
+
+#Split BMP file into pixel rows and process into binary
+def makePicture(path,width):
+    im = Image.open(path)
+    imag = list(im.getdata())
+    imag_rows = splitToRows(imag,width)
+    return processRows(imag_rows)
 
 #Take large BMP and partition it into image blocks for multi-block marquees
 def makeAllPics(path, widthTotal, sections):
     widthSection = widthTotal/sections
+    sectionList = []
     for i in range(0,sections):
-        makePicturePart(path,widthTotal,widthSection,sections-1-i)
+        sectionList.append(makePicturePart(path,widthTotal,widthSection,sections-1-i))
+    return sectionList
+
+def makeAnimation(pathList, widthTotal, sections):
+    frames = []
+    for path in pathList:
+        frames.append(makeAllPics(path, widthTotal, sections))
+    return frames
 
 makePicture("C:\\Users\\Public\\Arial_Letters2\H.bmp",8)
 makePicture("C:\\Users\\Public\\Arial_Letters2\A.bmp",8)
@@ -85,16 +102,26 @@ makePicture("C:\\Users\\Public\\Penis\\penis3.bmp",8)
 makePicture("C:\\Users\\Public\\Penis\\penis2.bmp",8)
 makePicture("C:\\Users\\Public\\Penis\\penis1.bmp",8)
 print
-makeAllPics("C:\\Users\\Public\\Penis\\penis_1.bmp", 40, 5)
+print makeAllPics("C:\\Users\\Public\\Penis\\penis_1.bmp", 40, 5)
 print
-makeAllPics("C:\\Users\\Public\\Penis\\penis_2.bmp", 40, 5)
+print makeAllPics("C:\\Users\\Public\\Penis\\penis_2.bmp", 40, 5)
 print
-makeAllPics("C:\\Users\\Public\\Penis\\penis_3.bmp", 40, 5)
+print makeAllPics("C:\\Users\\Public\\Penis\\penis_3.bmp", 40, 5)
 print
-makeAllPics("C:\\Users\\Public\\Penis\\penis_4.bmp", 40, 5)
+print makeAllPics("C:\\Users\\Public\\Penis\\penis_4.bmp", 40, 5)
 print
-makeAllPics("C:\\Users\\Public\\Penis\\penis_5.bmp", 40, 5)
+print makeAllPics("C:\\Users\\Public\\Penis\\penis_5.bmp", 40, 5)
 print
-makeAllPics("C:\\Users\\Public\\Penis\\penis_6.bmp", 40, 5)
+print makeAllPics("C:\\Users\\Public\\Penis\\penis_6.bmp", 40, 5)
 print
-makeAllPics("C:\\Users\\Public\\Penis\\penis_7.bmp", 40, 5)
+print makeAllPics("C:\\Users\\Public\\Penis\\penis_7.bmp", 40, 5)
+penisPathList = [
+    "C:\\Users\\Public\\Penis\\penis_1.bmp",
+    "C:\\Users\\Public\\Penis\\penis_2.bmp",
+    "C:\\Users\\Public\\Penis\\penis_3.bmp",
+    "C:\\Users\\Public\\Penis\\penis_4.bmp",
+    "C:\\Users\\Public\\Penis\\penis_5.bmp",
+    "C:\\Users\\Public\\Penis\\penis_6.bmp",
+    "C:\\Users\\Public\\Penis\\penis_7.bmp"
+]
+print makeAnimation(penisPathList, 40, 5)
