ae1bdc756dee3b943bf5f28a89a5f89e6d269155
vogonpoet42 <vogonpoet42@29f8fe4d-d83e-4efa-2f1a-eb354652e9c9>
2013-01-06 23:52:30 +0000

    Added functionality for PIC (PIC uses column-based addressing vs Arduino which uses row-based). Still a lot of organization work needs to be done.

--- Serial Sign Driver/bmp.py
+++ Serial Sign Driver/bmp.py
@@ -2,9 +2,9 @@ import Image
 
 rows = []
 #The rows on the marquee blocks are jumbled, and this is the mapping
-roworder = [6, 7, 5, 1, 4, 2, 3, 0]
-
-#########################Utility Functions####################
+#roworder = [0, 3, 2, 4, 1, 5, 7, 6]
+#roworder = [5, 0, 1, 4, 7, 6, 3, 2]
+roworder = [1, 2, 7, 6, 3, 0, 5, 4]
 
 #Convert array to binary number, LSB first
 def arrayToBinary(array):
@@ -14,23 +14,6 @@ def arrayToBinary(array):
         arraybinary = arraybinary + elem*position
         position = position*2
     return arraybinary
-
-#Takes the long list of the image pixels and splits it into individual rows
-def splitToRows(imag,width):
-    imag_rows = []
-    while imag:
-        imag_row = imag[:width]
-        imag_rows.append(imag_row)
-        imag = imag[width:]
-    return imag_rows
-
-def getPictureRows(path, width):
-    im = Image.open(path)
-    imag = list(im.getdata())
-    imag_rows = splitToRows(imag,width)
-    return imag_rows
-
-#########################Rural Functions#######################
 
 #Create binary number for individual rows
 def processRows(imag_rows):
@@ -63,15 +46,108 @@ def scrollEachRowOnce(imag_rows):
         new_imag_rows.append(new_imag_row)
     return new_imag_rows
 
+#Takes the long list of the image pixels and splits it into individual rows
+def splitToRows(imag,width):
+    imag_rows = []
+    while imag:
+        imag_row = imag[:width]
+        imag_rows.append(imag_row)
+        imag = imag[width:]
+    return imag_rows
+
 #Split section of BMP file into pixel rows and process into binary
 def makePicturePart(path,widthTotal,widthSection,sectionIndex):
-    imag_rows = getPictureRows(path, widthTotal)
+    im = Image.open(path)
+    imag = list(im.getdata())
+    imag_rows = splitToRows(imag,widthTotal)
     return processRowsPart(imag_rows,widthSection,sectionIndex)
+
+def getPictureRows(path, width):
+    im = Image.open(path)
+    imag = list(im.getdata())
+    imag_rows = splitToRows(imag,width)
+    return imag_rows
+
+def splitToCols(imag, height):
+    imag_cols = []
+    while imag:
+        imag_col = imag[:height]
+        imag_cols.append(imag_col)
+        imag = imag[height:]
+    return imag_cols
+
+def getPictureCols(path, height):
+    im = Image.open(path)
+    im = im.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_90)
+    imag = list(im.getdata())
+    imag_cols = splitToCols(imag,height)
+    return imag_cols
+
+def makeRealPicture(path, height):
+    return processRows(getPictureCols(path, height))
+
+def processColsPart(imag_cols, height, i):
+    cols = []
+    imag_cols = imag_cols[height*(i):height*(i+1)]
+    for imag_col in imag_cols:
+        new_imag_col = [ imag_col[i] for i in roworder ]
+        #print imag_row, "->", new_imag_row
+        colbinary = arrayToBinary(new_imag_col)
+        #print rowbinary
+        cols.append(colbinary)
+    return cols
+
+print "HIP"
+print getPictureCols("C:\\Users\\Public\\Penis\\penis_1.bmp", 8)
+
+def makeFrame(path, height, widthTotal, widthSection):
+    allCols = []
+    for i in range(0,widthTotal/widthSection):
+        allCols.append(processColsPart(getPictureCols(path, height),  height, i))
+    return allCols
+
+def makeFrameAnimation(pathList, height, widthTotal, widthSection):
+    allFrames = []
+    for path in pathList:
+        allFrames.append(makeFrame(path, height, widthTotal, widthSection))
+    return allFrames
+
+penisPathList = [
+    "C:\\Users\\Public\\Penis\\longpenis_1.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_2.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_3.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_4.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_5.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_6.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_7.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_8.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_9.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_10.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_11.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_10.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_9.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_8.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_7.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_6.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_5.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_4.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_3.bmp",
+    "C:\\Users\\Public\\Penis\\longpenis_2.bmp",
+]
+
+print "SEX"
+print makeFrameAnimation(penisPathList, 8, 80, 8)
+#print makeFrame("C:\\Users\\Public\\Penis\\penis_1.bmp", 8, 40, 8)
+#allFrames = []
+#allCols = []
+#for i in range(0,5):
+#    allCols.append(processColsPart(getPictureCols("C:\\Users\\Public\\Penis\\penis_1.bmp", 8),  8, i))
+#print allCols
+print "SEX"
 
 #Split BMP file into pixel rows and process into binary
 def makePicture(path,width):
-    imag_rows = getPictureRows(path, width)
-    return processRows(imag_rows)
+    return processRows(getPictureRows(path, width))
 
 def processBigImage(imag_rows, widthTotal, sections):
     widthSection = widthTotal/sections
@@ -82,11 +158,10 @@ def processBigImage(imag_rows, widthTotal, sections):
 
 #Take large BMP and partition it into image blocks for multi-block marquees
 def makeAllPics(path, widthTotal, sections):
-    imag_rows = getPictureRows(path, widthTotal)
     widthSection = widthTotal/sections
     sectionList = []
     for i in range(0,sections):
-        sectionList.append(processRowsPart(imag_rows,widthTotal,widthSection,sections-1-i))
+        sectionList.append(makePicturePart(path,widthTotal,widthSection,sections-1-i))
     return sectionList
 
 def makeAnimation(pathList, widthTotal, sections):
@@ -95,15 +170,23 @@ def makeAnimation(pathList, widthTotal, sections):
         frames.append(makeAllPics(path, widthTotal, sections))
     return frames
 
-def makeScrollAnimation(path, widthTotal, sections, scrollFrames):
+def makeScrollAnimation(path, widthTotal, sections):
     frames = []
     imag_rows = getPictureRows("C:\\Users\\Public\\Penis\\penis_1.bmp", widthTotal)
-    for i in range(0,scrollFrames):
+    for i in range(0,20):
         frames.append(processBigImage(imag_rows, widthTotal, sections))
         imag_rows = scrollEachRowOnce(imag_rows)
     return frames
 
-makePicture("C:\\Users\\Public\\Arial_Letters2\H.bmp",8)
+print makeAllPics("C:\\Users\\Public\\Penis\\penis_1.bmp", 40, 5)
+
+print
+print
+
+print makePicture("C:\\Users\\Public\\Arial_Letters2\\H.bmp",8)
+print
+print
+print makeRealPicture("C:\\Users\\Public\\Arial_Letters2\\H.bmp",8)
 makePicture("C:\\Users\\Public\\Arial_Letters2\A.bmp",8)
 makePicture("C:\\Users\\Public\\Arial_Letters2\P.bmp",8)
 makePicture("C:\\Users\\Public\\Arial_Letters2\P.bmp",8)
@@ -146,6 +229,6 @@ penisPathList = [
     "C:\\Users\\Public\\Penis\\penis_6.bmp",
     "C:\\Users\\Public\\Penis\\penis_7.bmp"
 ]
-print makeAnimation(penisPathList, 40, 5)
-print
-print makeScrollAnimation("C:\\Users\\Public\\Penis\\penis_1.bmp", 40, 5)
+#print makeAnimation(penisPathList, 40, 5)
+#print
+#print makeScrollAnimation("C:\\Users\\Public\\Penis\\penis_1.bmp", 40, 5)
