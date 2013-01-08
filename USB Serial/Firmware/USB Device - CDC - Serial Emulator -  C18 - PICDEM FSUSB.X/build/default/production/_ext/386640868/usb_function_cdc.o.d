14bd3a03f738efb6645c90f914d7d9d9e350d01a
vogonpoet42 <vogonpoet42@29f8fe4d-d83e-4efa-2f1a-eb354652e9c9>
2013-01-08 04:31:03 +0000

    Committed new MPLAB.X for USB Serial

---
+++ USB Serial/Firmware/USB Device - CDC - Serial Emulator -  C18 - PICDEM FSUSB.X/build/default/production/_ext/386640868/usb_function_cdc.o.d
@@ -0,0 +1,15 @@
+build/default/production/_ext/386640868/usb_function_cdc.o:  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/Libraries/USB/CDC\ Device\ Driver/usb_function_cdc.c  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/Libraries/Include/USB/usb_common.h  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/Libraries/Include/USB/usb_device.h  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/Libraries/Include/USB/usb_hal.h  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/Libraries/Include/USB/usb_function_cdc.h  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/Libraries/Include/USB/usb.h  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/Libraries/Include/GenericTypeDefs.h  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/Libraries/Include/Compiler.h  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/USB\ Serial/Firmware/usb_config.h  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/Libraries/Include/USB/usb_ch9.h  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/Libraries/Include/USB/usb_hal_pic18.h  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/USB\ Serial/Firmware/HardwareProfile.h  \
+	C:/Users/denwabangohan/Documents/Pic_Projects/USB\ Serial/Firmware/HardwareProfile\ -\ toothPIC\ FSUSB.h  \
+
